deef8ae99ce92739a8d3ba9aadcbef4bae6dfb0e
Theo McCray <tlmccraycray@gmail.com>
2023-06-22 09:27:39 -0600

    Add variables

---
+++ variables.js
@@ -0,0 +1,10 @@
+var name = "Theo"
+var lastName = "McCray"
+var middleName = "Louis"
+var age = 31
+var fingers = 10
+var tired = true
+var awake = false
+console.log(tired)
+console.log(fingers)
+console.log(middleName)
